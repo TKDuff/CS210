897b451d7fc73a12ca81d0f263d1e6a568dd99fe
TKDuff <94256721+TKDuff@users.noreply.github.com>
2021-11-19 17:39:25 +0000

    Add files via upload

---
+++ LabW08.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+public class LabW08 {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	
+	int count = 0; //determines size of qeueu array
+	String input = ""; //stores inputs
+	
+	while(in.hasNextLine()) { 						//takes input until stop
+		input = input + "+" + in.nextLine(); 		//store current input, adding + to note end
+		count++; 									//increase size of array
+		if(input.substring(input.length()-6, input.length()).equals("REMOVE")) { 		//check if remove command issued
+			count--; 		//removing an item means the size of the array can be reduced
+		}
+	}
+	input += "+"; //add + to end to signify end of input string
+	//System.out.println(input + " " + count);
+	
+	
+	Queue q = new Queue(count); //create queue using counter 
+	//this splits large input string into separate commands
+	int start = 0; 								//starting position of command
+	for(int i = 0; i < input.length();i++) {	//goes through each letter
+		if(input.charAt(i) == '+') {			//if letter is +
+			String command = input.substring(start, i);	//get substring of large input string, only taking between the 2 +, so "+INSERT this+" is now "INSERT this"
+			start = i+1; 						//increment starting point for next command
+			if(command.split(" ")[0].equals("INSERT")) { //check command instruction
+				q.i(command.split(" ")[1]);				//if insert split string to insert the word
+			} else if(command.split(" ")[0].equals("REMOVE")) {//check command instruction
+				q.r();										//remove front item from queue
+			}
+		}
+	}
+
+	System.out.println(q.get()); //gets middle item
+	}
+
+}
+	
+
+class Queue{
+	private int maxSize;
+	private String[] queArray;
+	private int front;
+	private int rear;
+	private int nItems;
+	
+	public Queue(int s) {
+		maxSize = s;
+		queArray = new String[maxSize];
+		front = 0;
+		rear = -1;
+		nItems = 0;
+	}
+	
+	public boolean i(String j) {
+		if(isFull()) return false;
+		if(rear == maxSize-1)
+			rear = -1;
+		rear++;
+		queArray[rear] = j;
+		nItems++;
+		return true;
+	}
+	
+	public String r() {
+		if(isEmpty()) return (String) null;
+		String temp = queArray[front];
+		front++;
+		if(front == maxSize)
+			front = 0;
+		nItems--;
+		return temp;
+	}
+	
+	public boolean isEmpty() {
+		return (nItems == 0);
+	}
+	
+	public boolean isFull() {
+		return (nItems == maxSize);
+	}
+	
+	public int size() {
+		return nItems;
+	}
+	
+	public String peekFront() {
+		return queArray[front];
+	}
+	
+	void list() {
+		for(String s:queArray) {
+			System.out.println(s);
+		}
+	}
+	
+	
+	public String get() {
+		if(nItems == 0) {return "empty";} //if empty string return empty
+		//System.out.println(front + " " + rear + " " + nItems);
+		if(nItems % 2 == 0) { //if number of items is even
+			return queArray[front+ (nItems/2)-1]; //divide N by 2 and subtract 1 to find the middle value
+		} else {
+			return queArray[front + nItems/2];//in case not even just divide by 2 to find middle value
+		}
+	} 
+	
+} 
