b4de794173983b36ab2e316cc8c8a5544f682133
TKDuff <94256721+TKDuff@users.noreply.github.com>
2021-11-19 17:59:58 +0000

    Update LabW08.java

--- LabW08.java
+++ LabW08.java
@@ -1,4 +1,11 @@
 import java.util.Scanner;
+
+/*
+manipulate a queue according to the given insert and remove commands and then output the string that is in the middle of the queue
+If there is an even number of strings in the queue, thus two middle strings, output the one which is nearest to the front.
+If a remove command is issued for an empty queue then nothing should happen
+*/
+
 public class LabW08 {
 
 	public static void main(String[] args) {
@@ -7,28 +14,27 @@ public class LabW08 {
 	int count = 0; //determines size of qeueu array
 	String input = ""; //stores inputs
 	
-	while(in.hasNextLine()) { 						//takes input until stop
-		input = input + "+" + in.nextLine(); 		//store current input, adding + to note end
+	while(in.hasNextLine()) { 						//takes input until ide stops taking input (ctrl-d on linux eclipse)
+		input = input + "+" + in.nextLine(); 				//store current input, adding + to note end
 		count++; 									//increase size of array
 		if(input.substring(input.length()-6, input.length()).equals("REMOVE")) { 		//check if remove command issued
 			count--; 		//removing an item means the size of the array can be reduced
 		}
 	}
 	input += "+"; //add + to end to signify end of input string
-	//System.out.println(input + " " + count);
-	
 	
 	Queue q = new Queue(count); //create queue using counter 
+		
 	//this splits large input string into separate commands
-	int start = 0; 								//starting position of command
+	int start = 0; 					//starting position of command
 	for(int i = 0; i < input.length();i++) {	//goes through each letter
-		if(input.charAt(i) == '+') {			//if letter is +
-			String command = input.substring(start, i);	//get substring of large input string, only taking between the 2 +, so "+INSERT this+" is now "INSERT this"
+		if(input.charAt(i) == '+') {			//if letter is '+'
+			String command = input.substring(start, i);	//take substring between the 2 +, "+INSERT this+" is now "INSERT this"
 			start = i+1; 						//increment starting point for next command
 			if(command.split(" ")[0].equals("INSERT")) { //check command instruction
-				q.i(command.split(" ")[1]);				//if insert split string to insert the word
+				q.insert(command.split(" ")[1]);				//if insert split string to insert the word
 			} else if(command.split(" ")[0].equals("REMOVE")) {//check command instruction
-				q.r();										//remove front item from queue
+				q.remove();							//remove front item from queue
 			}
 		}
 	}
@@ -54,7 +60,7 @@ class Queue{
 		nItems = 0;
 	}
 	
-	public boolean i(String j) {
+	public boolean insert(String j) {
 		if(isFull()) return false;
 		if(rear == maxSize-1)
 			rear = -1;
@@ -64,7 +70,7 @@ class Queue{
 		return true;
 	}
 	
-	public String r() {
+	public String remove() {
 		if(isEmpty()) return (String) null;
 		String temp = queArray[front];
 		front++;
@@ -89,17 +95,9 @@ class Queue{
 	public String peekFront() {
 		return queArray[front];
 	}
-	
-	void list() {
-		for(String s:queArray) {
-			System.out.println(s);
-		}
-	}
-	
-	
+		
 	public String get() {
 		if(nItems == 0) {return "empty";} //if empty string return empty
-		//System.out.println(front + " " + rear + " " + nItems);
 		if(nItems % 2 == 0) { //if number of items is even
 			return queArray[front+ (nItems/2)-1]; //divide N by 2 and subtract 1 to find the middle value
 		} else {
