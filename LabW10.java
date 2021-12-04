76acdc6752ee79f899250a418c2c064ddcb61798
TKDuff <94256721+TKDuff@users.noreply.github.com>
2021-12-04 17:37:14 +0000

    Update LabW10.java

--- LabW10.java
+++ LabW10.java
@@ -1,74 +1,74 @@
-
-//Write recursive method computes output of following function
-//f(n) = 2 if n = 1
-//f(n) = 4*f(n-1) - 3*n if n > 1
-/*
 import java.util.Scanner;
+
 public class LabW10 {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+
+		System.out.print("Select which function recursive function to pick: ");
+		int question = in.nextInt();
+		long param1;
+		double param2;
+		double param3;
 		
-		System.out.println(f(68));
+		switch(question) {
+		case 1:
+			param1 = in.nextInt();
+			System.out.println(function1(param1));
+			break;
+		case 2:
+			System.out.println("Enter the number of years");
+			param1 = in.nextInt();
+			System.out.println("Enter the bank balance");
+			param2 = in.nextDouble();
+			System.out.println("Enter the interest rate");
+			param3 = 1+in.nextDouble()/100;
+			System.out.println(String.format( "%.2f", function2(param1, param2, param3)));
+			break;
+		case 3:
+			param1 = in.nextInt();
+			System.out.println(String.format( "%.3f", (1-function3(param1))));	
+			break;
+		default:
+			System.out.println("Wrong input or something");
+		}
+	
 	}
 	
-	public static long f(long n) {
+	/*
+	 * Write recursive method computes output of following function
+	 * f(n) = 2 if n = 1
+	 * f(n) = 4*f(n-1) - 3*n if n > 1
+	*/
+	public static long function1(long n) {
 		if(n==1) {
 			return 2;
 		}
-		return 4*f(n-1) - 3*n;
+		return 4*function1(n-1) - 3*n;
 	}
 	
-}
-*/
-
-//Write recursive method, takes balance, interest rate, number of years and
-//output how much bank account will be worth after inputted years
-/*
-import java.util.Scanner;
-public class LabW10 {
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int y = in.nextInt();
-		double inte = 1+in.nextDouble()/100;
-		double amount = in.nextDouble();
-		
-		double awnser = (f(y, amount, inte));
-		System.out.println((double)Math.round(awnser * 100)/100);	
-		
-	}
-	public static double f(int y,double amount, double inte) {	
+	/* function 2
+	 * Write recursive method, takes balance, interest rate, number of years and
+	 * output how much bank account will be worth after inputed years 
+	*/
+	public static double function2(long y,double amount, double inte) {	
 		if(y==1) {
 			return amount * inte;
 		}
-		return f(y-1, amount, inte) * inte;
+		return function2(y-1, amount, inte) * inte;
 	}
+	
+	/* function 3
+	* Write recursive method that finds probability that in a class of n people
+	* some pair of them will have the same birthday
+	*/
+	public static double function3(long n) {
+		if(n<=1){
+            return 1;
+        } 
+        float number = (365-(n-1));
+        float value = number/365;
+        return (value*function3(n-1));
+    }
 	
 }
-*/
-
-
-//Write recursive method that finds probability that in a class of n people
-//some pair of them will have the same birthday
-/*
-3)
-import java.util.Scanner;
-public class LabW10 {
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println(f(3));
-	}
-	
-	public static double f(double n) {
-		if(n==1) {
-			return 0;
-		}
-		return f(n-1) + (n-1)/365;
-	}
-	
-}
-*/
